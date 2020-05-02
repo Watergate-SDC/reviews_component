fe4b49c2697ebf294bc258fe48383af922d7015b
Danny (vai) San <vai.quoc.san@gmail.com>
2020-05-02 11:03:17 -0700

    modal seems to be responsive based off screen size

--- client/src/components/searchbutton/SearchButton.jsx
+++ client/src/components/searchbutton/SearchButton.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
+
 
 export default function SearchButton(props) {
+  const searchIcon = <FontAwesomeIcon icon={faSearch}/>
   
   return (
     <div>
       <button className="review-search-button">
-        <span>Search</span>
+        <span><searchIcon /></span>
       </button>
     </div>
 
