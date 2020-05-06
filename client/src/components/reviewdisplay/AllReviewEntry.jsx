3c82fbfc2abd394f8cfac2ade21383a581c094d6
Danny (vai) San <vai.quoc.san@gmail.com>
2020-05-06 14:16:40 -0700

    styling for main page reviews pretty much completed. could use some touch up. Moving onto review rating selected reviews

--- client/src/components/reviewdisplay/AllReviewEntry.jsx
+++ client/src/components/reviewdisplay/AllReviewEntry.jsx
@@ -2,28 +2,6 @@ import React, { useState } from 'react';
 import ReviewEntries from './ReviewEntries.jsx';
 
 export default function AllReviewEntry({ reviewData }) {
-  // const [data, setData] = useState([])
-  // let AllReviewCondition = () => {
-  //   if(reviewData.reviews === undefined){
-  //     return (
-  //       <div></div>
-  //     )
-  //   } else {
-  //     return (
-  //       <div className="all-review-entry-container">
-  //       {
-  //         reviewData.reviews.map((review, index) => {
-  //           <Review review={review} key={index}/>
-  //         })
-  //       }
-  //     </div>
-  //     )
-  //   }
-  // }
-  // useEffect(() => {
-  //   console.log('insideUseEFFECT', data)
-  //   setData(reviewData)
-  // })
   return (
     <div className="all-review-entry-container">
       {reviewData.map((review, index) => (
--- client/src/components/reviewdisplay/Review.jsx
+++ client/src/components/reviewdisplay/Review.jsx
@@ -1,8 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Review({ post }) {
   console.log('post', post);
+
+  let homeQuestions = () => {
+    let { location, dislikes, likes } = post;
+    if (location && dislikes && likes) {
+      return (
+        <div className="home-content-product-question">
+          <div className="home-content-question-1">
+            <div>
+              <span className="home-content-question">Where are you from?</span>
+              <span className="home-content-answers">{location}</span>
+            </div>
+          </div>
+          <div className="home-content-question-2">
+            <div>
+              <span className="home-content-question">
+                In a few words, what did you like?
+              </span>
+              <span className="home-content-answers">{likes}</span>
+            </div>
+          </div>
+          <div className="home-content-question-3">
+            <div>
+              <span className="home-content-question">
+                In a few words, what didn't you like?
+              </span>
+              <span className="home-content-answers">{dislikes}</span>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div className="home-content-product-question-container"></div>;
+    }
+  };
+
+  let homeRecommendation = () => {
+    let { recommendation } = post;
+    if (recommendation === false) {
+      return (
+        <div className="review-home-recommendation-message">
+          <span className="review-recommendation-icon">
+            <FontAwesomeIcon icon={faTimesCircle} />
+            No,{' '}
+          </span>
+          <span> I do not recommend this product. </span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="review-home-recommendation-message">
+          <span className="review-recommendation-icon"><FontAwesomeIcon icon={faCheckCircle} />Yes, </span>
+          <span> I recommend this product. </span>
+        </div>
+      )
+    }
+  };
+
   return (
     <div className="all-review-entry-container">
       <div className="review-content-header">
@@ -15,6 +74,21 @@ export default function Review({ post }) {
           <span>{moment(post.created_at).fromNow()}</span>
         </div>
       </div>
+      <div className="reivew-home-title-container">
+        <div className="reivew-home-title">
+          <h3 className="review-home-title-h3">{post.title}</h3>
+        </div>
+      </div>
+      <div className="review-home-content-body-container">
+        <div className="review-home-content-body">{post.review}</div>
+      </div>
+      <div className="review-home-product-question-container">
+        {homeQuestions()}
+      </div>
+      <div className="review-home-recommendation">
+        {homeRecommendation()}
+      </div>
+      <hr className="search-line"></hr>
     </div>
   );
 }
